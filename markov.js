c6e105c93b717188c5f78eb60a573926c5b32be0
Brian-15 <bcirieco@gmail.com>
2021-10-18 14:46:07 -0700

    implemented MarkovMachine class

--- markov.js
+++ markov.js
@@ -17,13 +17,37 @@ class MarkovMachine {
    *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
 
   makeChains() {
-    // TODO
+    const chains = {};
+    for (let i = 0; i < this.words.length; i++) {
+      const currWord = this.words[i];
+      const nextWord = this.words[i + 1] !== undefined? this.words[i + 1]: null;
+
+      if (chains[currWord]) {
+        chains[currWord].push(nextWord);
+      } else {
+        chains[currWord] = [nextWord];
+      }
+    }
+
+    this.chains = chains;
   }
 
 
   /** return random text from chains */
 
   makeText(numWords = 100) {
-    // TODO
+  
+    let currWord = this.words[Math.floor(Math.random() * this.words.length)];
+    let text = '';
+    
+    for (let i = 1; i <= numWords; i++) {
+      text += `${currWord} `;
+      const nextWords = this.chains[currWord];
+      currWord = nextWords[Math.floor(Math.random()*nextWords.length)];
+      
+      if (currWord === null) break;
+    }
+
+    return text;
   }
 }
